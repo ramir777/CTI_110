2b5d94acfe39690adae1a05333747cf0ea96a8fb
ramir777 <150169386+ramir777@users.noreply.github.com>
2023-11-07 11:21:52 -0500

    First Upload

---
+++ P1LAB1_MALDONADORAMIR.py
@@ -0,0 +1,9 @@
+#Ramir Maldonado
+#11/7/2023
+#Basic input/Output
+
+#Get input from user
+name=input("Please enter your name: ")
+
+#Display output to the user
+print("Hello", name, "welcome to CTI-110!")
